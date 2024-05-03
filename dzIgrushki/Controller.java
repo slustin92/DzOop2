b47b8de03ad457b89d3f69fa5c797c598836ffd6
Sergey Lystin <slustin92@mail.ru>
2024-05-04 01:23:17 +0400

    .\Controller.java

--- dzIgrushki/Controller.java
+++ dzIgrushki/Controller.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,62 +10,82 @@ public class Controller {
 
 
 
-    public void create(String toysName, int quantity, double weight) {
-        dataService.addToys(toysName, quantity, weight);
-
-    }
+//    public void create(String toysName, int quantity, double weight) {
+//        dataService.addToys(toysName, quantity, weight);
+//
+//    }
 
     public void print(){
-     for (Toys toys : dataService.getAll()) {
-         System.out.println(toys);
-                }
+        if (dataService.getAll().isEmpty()){
+            System.out.println("Игрушки закончились.");
+        }else {
+            for (Toys toys : dataService.getAll()) {
+                System.out.println(toys);
+            }
+        }
     }
     public void raffle() throws IOException {
         Random rn = new Random();
-        int randomNum = rn.nextInt(1, dataService.getAll().size());
-        dataService.receivingToys(randomNum);
+        if (dataService.getAll().isEmpty()) {
+            System.out.println("Игрушки закончились:");
+        }else {
+            int randomNum = rn.nextInt(1, (dataService.getAll().size()+1));
+            dataService.receivingToys(randomNum);
 
-        Writer myWriter = new FileWriter("myfile.txt", true);
-        myWriter.write("Получена призовая игрушка: " + String.valueOf(dataService.receivingPrizeToys()) + '\n'+ '\n');
-        myWriter.close();
-        dataService.delk(randomNum);
+            Writer myWriter = new FileWriter("myfile.txt", true);
+            myWriter.write("Получена призовая игрушка: " + String.valueOf(dataService.receivingPrizeToys()) + '\n' + '\n');
+            myWriter.close();
+            dataService.delk(randomNum);
 
+        }
 
     }
 
     public void progra() throws IOException {
-        String meny = ("Выберите действие: " + '\n' +
-//                "1 - Добавить игрушку." + '\n' +
-                "1 - Вывести список игрушок." + '\n' +
-                "2 - Розыгрышь игрушок. " + '\n' +
-                "3 - Выход.");
+        String meny = ('\n' + "        Меню: " + '\n' +
+                "1 - Добавить игрушку." + '\n' +
+                "2 - Вывести список игрушок." + '\n' +
+                "3 - Розыгрышь игрушок. " + '\n' +
+                "4 - Задать вес для выпадения игрушки. " + '\n' +
+                "5 - Выход." + '\n');
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println(meny);
+
         boolean swi = true;
         while (swi){
-
+            System.out.println(meny);
+            System.out.print("Выберите действие: ");
             int i = scanner.nextInt();
+            dataService.ToysQuantity();
 //            scanner.close();
 
             switch (i) {
 
 
                 case 1:
-                    print();
+                    dataService.addToys();
                     break;
                 case 2:
-                    raffle();
+                    print();
                     break;
                 case 3:
+                    raffle();
+
+                    break;
+                case 4:
+
+                    dataService.weightToys();
+                    break;
+                case 5:
                     System.out.println("До свидания: ");
                     swi = false;
                     break;
 
 
 
+
             }
-            System.out.println('\n' + meny);
+//            System.out.println('\n' + meny);
 //            scanner.close();
         }
 
