cdade5c59bd777cd970aab7d06b267f4f999da77
Sergey Lystin <slustin92@mail.ru>
2024-05-04 01:24:19 +0400

    .\Main.java

--- dzIgrushki/Main.java
+++ dzIgrushki/Main.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,14 +7,16 @@ public class Main {
         Controller controller = new Controller();
 
 
-        controller.create("Мишка", 5, 0.1);
-        controller.create("Зайка", 5, 0.2);
-        controller.create("Котик", 5, 0.3);
-        controller.create("Машина", 5, 0.4);
-        controller.create("Трактор", 5, 0.7);
-        controller.create("Собачка", 5, 0.05);
+//        controller.create("Мишка", 5, 0.1);
+//        controller.create("Зайка", 5, 0.2);
+//        controller.create("Котик", 5, 0.3);
+//        controller.create("Машина", 5, 0.4);
+//        controller.create("Трактор", 5, 0.7);
+//        controller.create("Собачка", 5, 0.05);
 
         controller.progra();
+
+
     }
 
 
