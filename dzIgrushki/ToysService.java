e8eedd94967a1a520e8cc21f0388b8354faf2a00
Sergey Lystin <slustin92@mail.ru>
2024-05-04 01:23:47 +0400

    .\ToysService.java

--- dzIgrushki/ToysService.java
+++ dzIgrushki/ToysService.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -5,7 +8,7 @@ import java.util.Scanner;
 public class ToysService {
     private List<Toys> userList = new ArrayList<>();
     private List<Toys> prizeToys = new ArrayList<>();
-//    Toys toys = new Toys();
+    private Scanner sr = new Scanner(System.in);
 
 
     public ToysService() {
@@ -15,22 +18,34 @@ public class ToysService {
     public List<Toys> getAll() {
         return userList;
     }
-Scanner sr = new Scanner(System.in);
-
-    public void addToys(String toysName, int quantity, double weight) {
-
-
-        int countMaxId = 0;
-        for (Toys toys: userList){
-            if (toys.getToysId() > countMaxId){
-                countMaxId = toys.getToysId();
+    public void ToysQuantity() {
+        if (!userList.isEmpty()) {
+            for (Toys toys : userList) {
+                if (toys.getQuantity() < 1) {
+                    userList.remove(toys);
+                }
             }
         }
-        countMaxId++;
-        Toys toys = new Toys(countMaxId, toysName, quantity, weight);
-        userList.add(toys);
     }
-//
+
+public void addToys(){
+
+
+    System.out.print("Введите Id игрушки: ");
+    int id = sr.nextInt();
+    System.out.print("Введите имя игрушки: ");
+    String name = sr.next();
+    System.out.print("Введите кол-во игрушек: ");
+    int quantity = sr.nextInt();
+    System.out.print("Введите вес игрушки: ");
+    double weight = sr.nextDouble();
+
+
+    userList.add(new Toys(id, name, quantity, weight));
+    System.out.println('\n' + "Игрушка " + name + " добавлен.");
+
+
+}
 
     public void receivingToys(int rand){
         for (Toys toys: userList){
@@ -46,17 +61,32 @@ Scanner sr = new Scanner(System.in);
         return prizeToys.getFirst();
     }
 
-    public void delk(int s){
-        for (Toys to : userList){
-            if (to.getToysId() == s){
-                int i = to.getQuantity() -1;
+    public void delk(int s) {
+
+        for (Toys to : userList) {
+            if (to.getToysId() == s) {
+                int i = to.getQuantity() - 1;
                 to.setQuantity(i);
             }
         }
-
     }
 
+        public void weightToys () throws IOException {
+            System.out.print("Введите вес для выполения игрушек: ");
+            double weig = sr.nextDouble();
+
+            for (Toys toys : userList) {
+                if (toys.getWeight() == weig) {
+                    System.out.println(toys);
+                    prizeToys.add(toys);
+                    delk(toys.getToysId());
+                    Writer myWriter = new FileWriter("myfile.txt", true);
+                    myWriter.write("Получена призовая игрушка: " + String.valueOf(toys) + '\n' + '\n');
+                    myWriter.close();
+                }
+            }
+
+        }
 
 
-
-}
+    }
